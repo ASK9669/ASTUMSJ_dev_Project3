c1bfb02f81fadc1052d6c6f89ec4ba817a294ef7
Awel Surur <awelsurur69@gamil.com>
2026-07-12 09:38:21 +0300

    2nd edition

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,19 @@ import ColorPreview from "./components/ColorPreview";
 function App() {
   const [color, setColor] = useState("");
 
-  const colors = ["red", "blue", "green", "orange", "purple"];
+  const colors = ["red", "blue", "green", "orange", "violet", "yellow", "purple"];
 
   return (
     <div className="App" style={{ padding: 20 }}>
       <h1 style={{color:"black"}}>Color Palette picker</h1>
       <p>Pick a color to preview it below</p>
-      <h4 style={{ marginTop: 20, display:"flex", flexDirection: "row", marginLeft: 70 }}>Choose a color:</h4>
+      <h4 style={{ marginTop: 20, display:"flex", flexDirection: "row" }}>Choose a color:</h4>
         <div 
   style={{ 
-    display: "flex",flexDirection: "row", gap: 10,
-    alignItems: "center",
-    justifyContent: "center"
+    display: "flex",flexDirection: "row", gap: 10,alignItems: "center",justifyContent: "center"
   }}
 >
-  {colors.map((colorName) => (
-    <ColorButton
-      key={colorName}
-      color={colorName}
-      onClick={() => setColor(colorName)}
-    >
-      {colorName}
-    </ColorButton>
+  {colors.map((colorName) => (<ColorButton key={colorName} color={colorName} onClick={() => setColor(colorName)}>{colorName} </ColorButton>
   ))}
 </div>
         <div style={{ marginTop: 20, display: "flex", flexDirection: "column", alignItems: "center" }}>
@@ -38,7 +29,7 @@ function App() {
           </p>
         </div>
         <div>
-          <button style={{ marginTop: 20, border: "2px solid #ccc", backgroundColor: "#a99d9d" }} onClick={() => setColor("")}>
+          <button style={{ padding: 3, marginTop: 20, borderRadius: "5px", border: "2px solid #343232", backgroundColor: "#fbf2f2", color: "#000000" }} onClick={() => setColor("")}>
             🔄 Reset
           </button>
         </div>
